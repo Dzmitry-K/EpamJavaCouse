050139ccada410664147370a3b375b51129d42f2
Dzmitry K <dzmitry.karachun@mail.ru>
2019-10-01 17:03:37 +0300

    ADD second task

--- src/BasicsOfSoftwareCodeDevelopment/LinearPrograms/SecondTask/SecondTask.java
+++ src/BasicsOfSoftwareCodeDevelopment/LinearPrograms/SecondTask/SecondTask.java
@@ -1,4 +1,16 @@
 package BasicsOfSoftwareCodeDevelopment.LinearPrograms.SecondTask;
 
+/**
+ * Вычислить значение выражения по формуле (все переменные принимают
+ * действительные значения):
+ * (b + sqrt(b*b + 4ac))/2 - a*a*a*c + Math.pow(b, -2);
+ * */
 public class SecondTask {
+    public static void main(String[] args) {
+        System.out.println(functionValue(1,2,3));
+    }
+
+    static double functionValue(double a, double b, double c) {
+        return (b + Math.sqrt(b * b + 4 * a * c)) / (2 * a) - a * a * a * c + Math.pow(b,-2);
+    }
 }
