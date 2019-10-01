6be7230d225f49ba97b3ca9f82c75ba81415a146
Dzmitry K <dzmitry.karachun@mail.ru>
2019-10-01 16:43:53 +0300

    ADD first task

--- src/BasicsOfSoftwareCodeDevelopment/LinearPrograms/FirstProgram/Main.java
+++ src/BasicsOfSoftwareCodeDevelopment/LinearPrograms/FirstProgram/Main.java
@@ -1,15 +1,14 @@
 package BasicsOfSoftwareCodeDevelopment.LinearPrograms.FirstProgram;
+
 /**
  * Найдите значение функции: z=((a-3)*b/2)+c.
- * */
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println(functionValue(4,2,1));
+        System.out.println(functionValue(4, 2, 1));
     }
 
-
-    static double functionValue(int a, int b, int c){
+    static double functionValue(int a, int b, int c) {
         return ((((a - 3) * b) / 2) + c);
     }
-
 }
