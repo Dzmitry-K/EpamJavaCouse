95d6a0034b86f91ac0711fa562c9618b39801915
Dzmitry K <dzmitry.karachun@mail.ru>
2019-10-01 17:45:18 +0300

    FIX third task

--- src/BasicsOfSoftwareCodeDevelopment/LinearPrograms/ThirdTask/ThirdTask.java
+++ src/BasicsOfSoftwareCodeDevelopment/LinearPrograms/ThirdTask/ThirdTask.java
@@ -5,10 +5,10 @@ import static java.lang.Math.*;
 /**
  * Вычислить значение выражения по формуле (все переменные принимают действительные значения):
  * ((sin(x) + cos(y)) / (cos(x) - sin(y)) * tg(x * y)
- * */
+ */
 public class ThirdTask {
     public static void main(String[] args) {
-        System.out.println(functionValue(30,45));
+        System.out.println(functionValue(30, 45));
     }
 
     static double functionValue(double x, double y) {
