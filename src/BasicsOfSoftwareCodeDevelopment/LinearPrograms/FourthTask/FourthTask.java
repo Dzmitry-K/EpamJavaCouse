bbfc3885477d27e66674f8ba0faa5125ab1e6abd
Dzmitry K <dzmitry.karachun@mail.ru>
2019-10-11 15:27:13 +0300

    ADD fourth task

---
+++ src/BasicsOfSoftwareCodeDevelopment/LinearPrograms/FourthTask/FourthTask.java
@@ -0,0 +1,18 @@
+package BasicsOfSoftwareCodeDevelopment.LinearPrograms.FourthTask;
+
+/**
+ * Дано действильное число R вида nnn.ddd (три цифровых разряда в дробной и целой частях).
+ * Поменять местами дробную и целую части числа и вывести полученное значение числа.
+ */
+
+public class FourthTask {
+    static Double changeOfValues(double inputNumber) {
+        int nnn = (int) inputNumber;
+        int ddd = (int) ((inputNumber - nnn) * 1000);
+        return Double.parseDouble(ddd + "." + nnn);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(changeOfValues(123.456));
+    }
+}
