1f5edcde0e234b9c8f95316cb5603c0245615968
Dzmitry K <dzmitry.karachun@mail.ru>
2019-10-11 17:48:44 +0300

    ADD first_cycles_task

---
+++ src/BasicsOfSoftwareCodeDevelopment/Cycles/FirstTask/FirstCyclesTask.java
@@ -0,0 +1,18 @@
+package BasicsOfSoftwareCodeDevelopment.Cycles.FirstTask;
+/**
+ * Напишите программу, где пользователь вводит любое целое положительное число.
+ * А программа суммирует все числа от 1 до введенного пользователем числа.
+ */
+public class FirstCyclesTask {
+    static int summ(int x) {
+        int result = 0;
+        for (int i = 1; i <= x; i++) {
+            result += i;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(summ(5));
+    }
+}
