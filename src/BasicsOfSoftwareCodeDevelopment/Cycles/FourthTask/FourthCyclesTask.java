70cabfbd2a19ca6347171e3b20a07131adfa4aac
dzmitry.karachun@mail.ru <6DFyAnb5>
2019-10-13 11:40:27 +0300

    ADD fourth_cycles_task

---
+++ src/BasicsOfSoftwareCodeDevelopment/Cycles/FourthTask/FourthCyclesTask.java
@@ -0,0 +1,22 @@
+package BasicsOfSoftwareCodeDevelopment.Cycles.FourthTask;
+
+import java.math.BigInteger;
+/**
+ * Составить программу нахождения произведения квадратов первых двухсот чисел.
+ */
+public class FourthCyclesTask {
+    static BigInteger squareMultiplication(){
+        BigInteger res = BigInteger.valueOf(1);
+        BigInteger pre_res;
+        for (int i = 2; i < 20; i++) {
+            pre_res = BigInteger.valueOf(i).multiply(BigInteger.valueOf(i));
+            res = res.multiply(pre_res);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(squareMultiplication());
+    }
+
+}
