6567dcaaccc34769b654ecf5b6a79c4d32c9e002
Dzmitry K <dzmitry.karachun@mail.ru>
2020-01-03 16:23:33 +0300

    !

--- src/Algorithmization/Multi_Array/Task_3/Task_3.java
+++ src/Algorithmization/Multi_Array/Task_3/Task_3.java
@@ -1,6 +1,8 @@
 package Algorithmization.Multi_Array.Task_3;
 import Algorithmization.Multi_Array.Task_1.Task_1;
 
+import java.util.Scanner;
+
 /**
  *  Дана матрица. Вывести k-ю строку и p-й столбец матрицы.
  */
@@ -10,5 +12,22 @@ public class Task_3 {
         int[][] array = Task_1.createMatrix();
         Task_1.fillingMatrix(array);
         Task_1.showMatrix(array);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter line number(k): ");
+        int k = scanner.nextInt();
+        System.out.print("Enter column number(p): ");
+        int p = scanner.nextInt();
+        System.out.println("--------------------------------------------");
+
+        System.out.print("Line №" + k + ":  ");
+        for (int j = 0; j < array[k].length; j++) {
+            System.out.printf("%3d ", array[k - 1][j]);
+        }
+        System.out.println("\n--------------------------------------------");
+        System.out.printf("Column №%d:  %d%n", p, array[0][p - 1]);
+        for (int i = 1; i < array.length; i++) {
+            System.out.printf("%15d%n", array[i][p - 1]);
+        }
     }
 }
